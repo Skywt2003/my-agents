b449eb41ee3b29806c68e3be5cc80c2654b4303d
SkyWT <me@skywt.net>
2026-07-28 12:04:42 +0000

    Allow text selection in the conversation view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col overflow-hidden">
+      <body className="min-h-full flex select-none flex-col overflow-hidden">
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
